5b560ffe718dfb1a18eb07921cad3367da827c34
Zelalem <zelalem@Zola.Zola>
2024-11-23 12:59:16 +0300

    create new form

--- components/JobForm.tsx
+++ components/JobForm.tsx
@@ -194,6 +194,7 @@ const JobForm = () => {
           className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
         />
       </div>
+      
 
       <button
         type="submit"
